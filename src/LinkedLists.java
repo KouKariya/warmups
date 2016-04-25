7ad84ca18a6b04dd9a79ff7c9f5a3ad3e7d270fa
KouKariya <jssgallarzo@gmail.com>
2016-04-25 11:10:19 -0500

    Updated LinkedLists class that now includes isEmpty and size methods to give more information regarding the LinkedLists object

--- src/LinkedLists.java
+++ src/LinkedLists.java
@@ -31,6 +31,7 @@ public class LinkedLists {
 
     }
 
+
     //Reference to the first node in the list
     //'first' is the name of the first node that is created when the
     //LinkedLists class is created
@@ -41,36 +42,59 @@ public class LinkedLists {
     builds a linked list
      */
     public LinkedLists() {
-        //Incrementally build the list
 
-        //Chuck Debby Elaine Fred
-        first = new Node("Debby");
-        first.next = new Node("Elaine");
-        first.next.next = new Node("Fred");
-        first = new Node("Chuck", first);
 
-        String[] names = {"Bob", "Allen"};
-
-        /*
-        Use a loop to add all names in the array to the
-        front of the linked list to build the list
-        Allan Bob Chuck Debby Elaine Fred
-         */
-        for (String s : names) {
-            first = new Node(s, first);
-        }
+//        //Incrementally build the list(first example)
+//        //Chuck Debby Elaine Fred
+//        first = new Node("Debby");
+//        first.next = new Node("Elaine");
+//        first.next.next = new Node("Fred");
+//        first = new Node("Chuck", first);
+//
+//        String[] names = {"Bob", "Allen"};
+//
+//        /*
+//        Use a loop to add all names in the array to the
+//        front of the linked list to build the list
+//        Allan Bob Chuck Debby Elaine Fred
+//         */
+//        for (String s : names) {
+//            first = new Node(s, first);
+//        }
     }
 
-        /*
-        The print method traverses the list
-        and prints all of its elements
-         */
-        public void print(){
-            Node ref = first;
-            while(ref != null){
-                System.out.println(ref.value + " ");
-                ref = ref.next;
-            }
+    /*
+    The isEmpty method detects whether the linked list is empty
+    @return true if list is empty, false otherwise.
+    */
+    public boolean isEmpty(){
+        return first == null;
+    }
+
+    /*
+    The size method returns the length of the list.
+    @return The number of elements in the list.
+     */
+    public int size(){
+        int count = 0;
+        Node p = first;
+        while(p != null){
+            count ++;
+            p = p.next;
+        }
+        return count;
+    }
+
+    /*
+    The print method traverses the list
+    and prints all of its elements
+    */
+    public void print(){
+        Node ref = first;
+        while(ref != null){
+            System.out.println(ref.value + " ");
+            ref = ref.next;
+        }
 
     }
 
