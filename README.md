f89973dd84930466e4a0c4d43416bf1996e0aeba
KouKariya <jssgallarzo@gmail.com>
2016-11-21 17:58:37 -0600

    Added PassString.java. Updated README.md to reflect current projects.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # warmups
-This repo contains a series of warmup classes intended for educational use and practice. All code is either a modified or obtained from the textbook Starting Out with Java: From Control Structures through Data Structures 2nd Edition.
+This repo contains a series of warmup classes intended for educational use and practice. All code is either a modified or obtained from the textbook "Starting Out with Java: From Control Structures through Data Structures 2nd Edition."
+Unless specified otherwise.
 
-Linkedlists.java
+#Linkedlists.java
 Shows a working example of how to build a Linked List without calling the built in LinkedList object.
 
+#CreditCard.java
+Shows how to use methods in Java.
 
+#PassString.java
+More method uses for Java. Shows how String variables are immutable.
---
+++ src/PassString.java
@@ -0,0 +1,32 @@
+/**
+ * PassString.java
+ * Created by kariya on 11/21/2016.
+ * This program demonstrates that String arguments cannot be changed.
+ */
+public class PassString {
+    public static void main(String[] args){
+        //Create a String object containing "Shakespeare"
+        //The name variable references the object.
+        String name = "Shakespeare";
+
+        //Display the String referenced by the name vairable.
+        System.out.println("In main, the name is "+ name);
+
+        //Call the changeName method, passing the contents of the name variable as an argument.
+        changeName(name);
+
+        //Display the String referenced by the name variable.
+        System.out.println("Back in main, the name is "+ name);
+    }
+    /**
+     * The changeName method accepts a String as its argument and assigns the str parameter to a new String
+     */
+    public static void changeName(String str){
+        //Create a String object containing "Dickens"
+        str = "Dickens";
+
+        //Display the String referenced by str.
+        System.out.println("In changeName, the name is now "+ str);
+    }
+
+}
