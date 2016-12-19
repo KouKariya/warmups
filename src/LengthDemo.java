93622e821f4699ca3a31d5a8750992a975c42394
KouKariya <jssgallarzo@gmail.com>
2016-12-19 10:40:04 -0600

    Rectangle.java and LengthDemo.java added

---
+++ src/LengthDemo.java
@@ -0,0 +1,8 @@
+/**
+ * LengthDemo.java
+ * Created by kariya on 12/19/2016.
+ * This program demonstrates the Rectangle class' setLength method.
+ */
+public class LengthDemo {
+
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,16 @@
+/**
+ * Rectangle class, phase 1
+ * Under construction!
+ * Created by kariya on 12/19/2016.
+ */
+public class Rectangle {
+    private double length;
+    private double width;
+
+    /**
+     * The setLength method stores a value in the length field.
+     */
+    public void setLength(double len){
+        length = len;
+    }
+}
