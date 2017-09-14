432a55eb25df134d7a94c70724aef7e134a843cb
Jesse Gallarzo <jssgallarzo@gmail.com>
2017-09-14 01:09:43 -0500

    Added Exercises.java

---
+++ src/Exercises.java
@@ -0,0 +1,14 @@
+/**
+ * Exercises.java
+ This program runs several practice exercises.
+ */
+public class Exercises {
+    public static void main(String[] args){
+
+        int value;
+
+        value = 5;
+        System.out.println("The value is ");
+        System.out.println(value);
+    }
+}
