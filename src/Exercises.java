de2324b6309b4808cc72735f8d52893cc1064117
Jesse Gallarzo <jssgallarzo@gmail.com>
2017-09-19 00:00:40 -0500

    Adding code to test within Exercises.java

--- src/Exercises.java
+++ src/Exercises.java
@@ -3,16 +3,29 @@
  This program runs several practice exercises.
  */
 public class Exercises {
+    /**
+     * exerciseMethod
+     * Method that takes several different double values and
+     * returns the calculated wage for someone for the week
+     */
+    public static void exerciseMethod(){
+        double regularWages;    //The calculated regular wages.
+        double basePay = 25;    //The base pay rate.
+        double regularHours = 40;   //The hours worked less overtime.
+        double overtimeWages;   //Overtime wages.
+        double overtimePay = 37.5;   //Overtime pay rate
+        double overtimeHours = 10;  //Overtime hours worked.
+        double totalWages;  //Total wages.
+
+        regularWages = basePay * regularHours;
+        overtimeWages = overtimePay * overtimeHours;
+        totalWages = regularWages + overtimeWages;
+
+        System.out.println("Wages for this week are $" + totalWages);
+    }
+
     public static void main(String[] args){
 
-        int value;
-
-        value = 5;
-        System.out.println("The value is ");
-        System.out.println(value);
-
-        System.out.println("Enter a value that is " +
-                "greater than zero and less " +
-                "than 10.");
+       exerciseMethod();
     }
 }
